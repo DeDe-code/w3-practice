c514a2735fe48da47be420b8f287b1a524699330
DeDe-code <derzsidezso@gmail.com>
2023-02-16 15:41:56 +0200

    added for loops

---
+++ frontend/js/script.js
@@ -0,0 +1,22 @@
+let favoriteBooks = [
+  {
+    title: "Öreg néne őzikéje",
+    author: "Fazekas Anna",
+    year: 1952,
+    isNewerThan2000: false,
+  },
+  {
+    title: "Szamárfül",
+    author: "Romhányi József",
+    year: 2003,
+    isNewerThan2000: true,
+  },
+];
+
+for (const book of favoriteBooks) {
+  console.log(book.title);
+}
+
+for (let i = 0; i < favoriteBooks.length; i++) {
+  console.log(favoriteBooks[i].author);
+}
